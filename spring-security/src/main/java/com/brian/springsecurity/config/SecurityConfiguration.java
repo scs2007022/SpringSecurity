8af9b67fdf8fd61b45615e2024dda29306861fc7
Brian Lo <brian.lo@wisespotgroup.com>
2023-07-11 12:50:46 +0800

    SecurityConfiguration

---
+++ spring-security/src/main/java/com/brian/springsecurity/config/SecurityConfiguration.java
@@ -0,0 +1,31 @@
+package com.brian.springsecurity.config;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+            .csrf(Customizer.withDefaults())
+            .authorizeHttpRequests((auth)->auth.requestMatchers("").permitAll().anyRequest().authenticated())
+            .sessionManagement((sess)->sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authenticationProvider(authenticationProvider)
+            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
