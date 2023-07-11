36e090adeaf7ace9d1e5eda86bd86f68b12eba79
BrianLo <66210536+scs2007022@users.noreply.github.com>
2023-07-11 08:47:11 +0800

    .

--- spring-security/src/main/java/com/brian/springsecurity/config/JwtService.java
+++ spring-security/src/main/java/com/brian/springsecurity/config/JwtService.java
@@ -1,14 +1,17 @@
 package com.brian.springsecurity.config;
 
+import org.springframework.boot.autoconfigure.ssl.SslBundleProperties.Key;
 import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 
 @Service
 public class JwtService {
 
-    private static final String SECRET_KEY = "";
+    private static final String SECRET_KEY = "FIv3CBYaZNmJSAqaR+VLQiWEu0bPgG1TD5whbk7RO9kc+8RJ3EJpxa4acv9YD09C";
 
     public String extractUsername(String jwt) {
         return null;
@@ -16,5 +19,10 @@ public class JwtService {
 
     private Claims extractAllClaims(String jwt){
         return Jwts.parserBuilder().setSigningKey(getSigningKey()).build().parseClaimJws(jwt).getBody();
+    }
+
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
