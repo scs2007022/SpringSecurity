9b77943c3d78141114087288c0561eeeb83354ba
Brian Lo <brian.lo@wisespotgroup.com>
2023-07-11 10:46:59 +0800

    .

--- spring-security/src/main/java/com/brian/springsecurity/config/JwtService.java
+++ spring-security/src/main/java/com/brian/springsecurity/config/JwtService.java
@@ -29,7 +29,24 @@ public class JwtService {
         final Claims claims = extractAllClaims(jwt);
         return claimsResolver.apply(claims);
     }
+    
+    public boolean isTokenValid(String jwt, UserDetails userDetails){
+        final String username = extractUsername(jwt);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(jwt);
+    }
 
+    private boolean isTokenExpired(String jwt) {
+        return extractExpiration(jwt).before(new Date());
+    }
+    
+    private Date extractExpiration(String jwt) {
+        return extractClaim(jwt, Claims::getExpiration);
+    }
+    
+    private Claims extractAllClaims(String jwt){
+        return Jwts.parserBuilder().setSigningKey(getSignInKey()).build().parseClaimsJws(jwt).getBody();
+    }
+    
     public String generateJwt(UserDetails userDetails){
         return generateJwt(new HashMap<>(), userDetails);
     }
@@ -42,23 +59,6 @@ public class JwtService {
         .setExpiration(new Date(System.currentTimeMillis()+3*24*60*60))
         .signWith(getSignInKey(),SignatureAlgorithm.HS256)
         .compact();
-    }
-
-    public boolean isTokenValid(String jwt, UserDetails userDetails){
-        final String username = extractUsername(jwt);
-        return (username.equals(userDetails.getUsername())) && !isTokenExpired(jwt);
-    }
-
-    private boolean isTokenExpired(String jwt) {
-        return extractExpiration(jwt).before(new Date());
-    }
-
-    private Date extractExpiration(String jwt) {
-        return extractClaim(jwt, Claims::getExpiration);
-    }
-
-    private Claims extractAllClaims(String jwt){
-        return Jwts.parserBuilder().setSigningKey(getSignInKey()).build().parseClaimsJws(jwt).getBody();
     }
 
     private Key getSignInKey() {
