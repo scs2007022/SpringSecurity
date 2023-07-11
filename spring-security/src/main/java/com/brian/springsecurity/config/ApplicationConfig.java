3e7d41529de3fb9cc83fe74fd30b02e8a473a595
Brian Lo <brian.lo@wisespotgroup.com>
2023-07-11 10:54:07 +0800

    ApplicationConfig

---
+++ spring-security/src/main/java/com/brian/springsecurity/config/ApplicationConfig.java
@@ -0,0 +1,8 @@
+package com.brian.springsecurity.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfig {
+    
+}
--- spring-security/src/main/java/com/brian/springsecurity/config/JwtAuthenticationFilter.java
+++ spring-security/src/main/java/com/brian/springsecurity/config/JwtAuthenticationFilter.java
@@ -3,6 +3,9 @@ package com.brian.springsecurity.config;
 import java.io.IOException;
 
 import org.springframework.lang.NonNull;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -17,11 +20,13 @@ import lombok.RequiredArgsConstructor;
 public class JwtAuthenticationFilter extends OncePerRequestFilter{
     
     private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
 
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException,IOException{
         final String authHeader = request.getHeader("Authorization");
         final String jwt;
+        final String userEmail;
         if (authHeader == null || !authHeader.startsWith("Bearer ")){
             filterChain.doFilter(request,response);
             return;
@@ -30,5 +35,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter{
         jwt = authHeader.substring(7);
         
         userEmail = jwtService.extractUsername(jwt);
+        if (userEmail!=null && SecurityContextHolder.getContext().getAuthentication()==null){
+            UserDetails userDetails = userDetailsService.loadUserByUsername(userEmail);
+        }
     }
 }
