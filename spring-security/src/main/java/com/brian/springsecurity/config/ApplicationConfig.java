f4c5c65f9e14ca13a9dd02f6d4468f2e07724a93
Brian Lo <brian.lo@wisespotgroup.com>
2023-07-11 11:38:42 +0800

    JwtAuthenticationFilter is done.

--- spring-security/src/main/java/com/brian/springsecurity/config/ApplicationConfig.java
+++ spring-security/src/main/java/com/brian/springsecurity/config/ApplicationConfig.java
@@ -1,8 +1,22 @@
 package com.brian.springsecurity.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.brian.springsecurity.repository.CustomerRepository;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
+@RequiredArgsConstructor
 public class ApplicationConfig {
     
+    private final CustomerRepository customerRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return username -> customerRepository.findByEmail(username).orElseThrow(()->new UsernameNotFoundException("User not found."));
+    }
 }
--- spring-security/src/main/java/com/brian/springsecurity/config/JwtAuthenticationFilter.java
+++ spring-security/src/main/java/com/brian/springsecurity/config/JwtAuthenticationFilter.java
@@ -3,9 +3,11 @@ package com.brian.springsecurity.config;
 import java.io.IOException;
 
 import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -37,6 +39,12 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter{
         userEmail = jwtService.extractUsername(jwt);
         if (userEmail!=null && SecurityContextHolder.getContext().getAuthentication()==null){
             UserDetails userDetails = userDetailsService.loadUserByUsername(userEmail);
+            if (jwtService.isTokenValid(jwt, userDetails)){
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
         }
+        filterChain.doFilter(request, response);
     }
 }
