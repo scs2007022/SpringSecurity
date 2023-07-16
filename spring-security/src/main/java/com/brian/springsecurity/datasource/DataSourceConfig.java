473c140721732d45d1bac42509b0df0b3b06e073
BrianLo <66210536+scs2007022@users.noreply.github.com>
2023-07-16 21:33:43 +0800

    .

--- spring-security/src/main/java/com/brian/springsecurity/datasource/DataSourceConfig.java
+++ spring-security/src/main/java/com/brian/springsecurity/datasource/DataSourceConfig.java
@@ -2,7 +2,6 @@ package com.brian.springsecurity.datasource;
 
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
