c78798cfd5c07acaac304da979d858249efceec9
Brian Lo <brian.lo@wisespotgroup.com>
2023-07-21 17:55:48 +0800

    .

--- spring-security/src/main/java/com/brian/springsecurity/secutiryconfig/AccessControllByRole.java
+++ spring-security/src/main/java/com/brian/springsecurity/secutiryconfig/AccessControllByRole.java
@@ -1,7 +1,5 @@
 package com.brian.springsecurity.secutiryconfig;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
